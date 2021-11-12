cefca2903fed8d3a3a7f71a9cdea0339199618cf
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-12 18:15:18 +0100

    [WIP] create product, cart & discount_code models

--- cart/models.py
+++ cart/models.py
@@ -2,3 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class Cart(models.Model):
+    """ Model to represent shopping cart """
+    cart_id = models.BigAutoField()
+    username = models.OneToOneField("User")
+    discount_code = models.ManyToOneRel("DiscountCode")
+    products = models.ManyToManyRel()
+
+class DiscountCode(models.Model):
+    """ Model to represent discount codes """
+    code = models.BigAutoField()
+    discount = models.IntegerField()
--- products/models.py
+++ products/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Product(models.Model):
+    """ Model to represent products """
+    product_id = models.BigAutoField()
+    name = models.CharField(null=False)
+    description = models.TextField(null=True)
+    price = models.FloatField(default=0.0, min=0.0)
+    stock = models.IntegerField(default=0, min=0)
+    img_path = models.ImageField()  # Use CharField to store img_path?
