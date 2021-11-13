58504355dcdb088782940a71a7a8ff38cdfb80cf
Gaston Giacobini <lu0210035@gmail.com>
2021-11-13 13:56:12 -0300

    Cart views created

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from cart.models import Cart_item
 
 # Create your views here.
+def cart_view(request):
+    cart = Cart_item.objects.all()    
+    return render(request, 'cart.html', {'cart': cart})
+        
+
+def delete_cart_item(request, cart_item_id):
+    cart_item = Cart_item.objects.get(cart_item_id=cart_item_id)
+    cart_item.delete()
+    return redirect('cart')
+
