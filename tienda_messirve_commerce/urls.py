72f49f8b821bee5419c44aae15f7759720de4f3d
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-13 11:30:41 +0100

    sort imports and good practices

--- tienda_messirve_commerce/urls.py
+++ tienda_messirve_commerce/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
-from django.conf import settings
-
-from usuarios.views import login_view, register_view, Home
+from usuarios.views import Home
+from usuarios.views import login_view
+from usuarios.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.models import User
 
 from django.views import generic
