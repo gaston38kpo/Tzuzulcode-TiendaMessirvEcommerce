cdd04ec4826d553f5c32695e301fd19c4514b820
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-14 12:01:43 +0100

    manage errors

--- cart/views.py
+++ cart/views.py
@@ -3,26 +3,33 @@ from .models import Cart
 from .models import CartProduct
 from products.models import Product
 
+# Errors 
+from django.http import HttpResponseBadRequest
+from django.http import HttpResponseNotAllowed
+from django.http import HttpResponseServerError
+from django.http import HttpResponseNotFound
+
 # Create your views here.
 
 
 def add_product_to_cart(request, product_id):
     redirect_url = request.META.get('HTTP_REFERER', 'home')
+    error_template = "ERROR: {0}. <a href=\"/home/\">HOME</a>"
 
-    # TODO: 405 Method Not Allowed
-    if not request.method == "POST":
-        return redirect(redirect_url)
+    # 405 Method Not Allowed
+    if not request.method == "POST":        
+        return HttpResponseNotAllowed(error_template.format("only POST method allowed"))
 
-    # TODO: unautheticated user with cart
+    # unautheticated user with cart
     if not request.user.is_authenticated:
-        return redirect(redirect_url)
+        return HttpResponseBadRequest(error_template.format( "login required"))
 
     # Get cart
     cart = Cart.objects.filter(user=request.user)
 
-    # TODO: 500 user doesn't have cart
+    # 500 user doesn't have cart
     if not cart:
-        return redirect(redirect_url)
+        return HttpResponseServerError(error_template.format("user doesn't have cart"))
 
     cart = cart[0]
 
@@ -35,23 +42,23 @@ def add_product_to_cart(request, product_id):
         product_is_already_in_cart = False
         product = Product.objects.filter(id=product_id)
 
-    # TODO: 404 Product not found
+    # 404 Product not found
     if not product:
-        return redirect(redirect_url)
+        return HttpResponseNotFound(error_template.format("product not found"))
 
     product = product[0]
     quantity_to_add = int(request.POST["quantity"])
 
-    # TODO: 400 Bad Request (invalid quantity)
+    # 400 Bad Request (invalid quantity)
     if quantity_to_add <= 0:
-        return redirect(redirect_url)
+        return HttpResponseBadRequest(error_template.format("invalid quantity, quantity must be a positive integer."))
 
     if product_is_already_in_cart:
         cart_product = CartProduct.objects.filter(cart_fk=cart, product_fk=product).get()
         total_quantity = cart_product.quantity + quantity_to_add
-        # TODO: 400 Bad Request (invalid total quantity)
+        # 400 Bad Request (invalid total quantity)
         if total_quantity > product.stock:
-            return redirect(redirect_url)
+            return HttpResponseBadRequest(error_template.format("can't set more quantity than current stock."))
         cart_product.quantity = total_quantity
     else:
         cart_product = CartProduct(
