e9f5b198033931280dd290a20602dc3fc64a5bca
Guillermo Gilpérez Alcázar <ggilperezalcazar@gmail.com>
2021-11-14 11:45:33 +0100

    do checks

--- cart/views.py
+++ cart/views.py
@@ -5,16 +5,22 @@ from products.models import Product
 
 # Create your views here.
 
+
 def add_product_to_cart(request, product_id):
     redirect_url = request.META.get('HTTP_REFERER', 'home')
+
+    # TODO: 405 Method Not Allowed
     if not request.method == "POST":
         return redirect(redirect_url)
 
-    if not  request.user.is_authenticated:
+    # TODO: unautheticated user with cart
+    if not request.user.is_authenticated:
         return redirect(redirect_url)
 
+    # Get cart
     cart = Cart.objects.filter(user=request.user)
 
+    # TODO: 500 user doesn't have cart
     if not cart:
         return redirect(redirect_url)
 
@@ -22,10 +28,16 @@ def add_product_to_cart(request, product_id):
     
     product = Product.objects.filter(id=product_id)
 
+    # TODO: 404 Product not found
     if not product:
         return redirect(redirect_url)
 
     product = product[0]
+    quantity_to_add = int(request.POST["quantity"])
+
+    # TODO: 400 Bad Request (invalid quantity)
+    if quantity_to_add <= 0:
+        return redirect(redirect_url)
 
     cart_product = CartProduct(
         cart_fk=cart,
